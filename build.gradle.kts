bae1862ef0f98568c1458a85d6670846d2ca88be
AndroidDeveloperLB <lblb636@gmail.com>
2026-02-19 03:45:01 +0200

    Trying to fix things for Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     id("com.android.application") version "9.0.1" apply false
     id("com.android.library") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
 
 //allprojects {
